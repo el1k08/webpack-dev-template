0fdc150d57689b78dbdeb94dbc0e60c13e7f0a23
el1k08 <eli@slavutski.com>
2020-09-02 13:30:13 +0300

    adding babeljs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './js/common'
+
+
+
+/* Examples of inputs */
+
+//import 'vue'
+//import 'bootstrap/dist/css/botstrap.min.css'
+//import Bootstrap from 'bootstrap/dist/css/botstrap.min.css'
+//import Vue from 'vue'
+// window.Vue = require('vue')
